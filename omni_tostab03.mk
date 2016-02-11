42a425cfcf4ee4609036c228978de7a32498ea22
TheMeddlingMonk <jwhite88@gmail.com>
2016-02-11 14:32:48 +1100

    omni_tostab03.mk

---
+++ omni_tostab03.mk
@@ -0,0 +1,10 @@
+#
+# Copyright header and licence details goes here (need to work out what this should be)
+#
+
+# Product details
+PRODUCT_NAME := omni_tostab03
+PRODUCT_DEVICE := tostab03
+PRODUCT_MODEL := AT100
+PRODUCT_BRAND := toshiba
+PRODUCT_MANUFACTURER := Toshiba

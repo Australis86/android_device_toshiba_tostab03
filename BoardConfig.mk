9952244265f771da53bff538163c7649924e3865
TheMeddlingMonk <jwhite88@gmail.com>
2016-02-12 20:20:22 +1100

    Add kernel source flags.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,9 +33,9 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 # Kernel
 BOARD_KERNEL_CMDLINE := nvmem=128M@384M mem=1024M@0M vmalloc=256M video=tegrafb tegra_fbmem=0x3e8a00@0x1fbfa000 console=ttyS0,115200n8 debug_uartport=hsport usbcore.old_scheme_first=1 lp0_vec=8192@0x1fbee000 tegraboot=sdmmc board_info=4249:ff41:ff:ff:54 gpt androidboot.carrier=wifi-only
 BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_PAGESIZE :=
-TARGET_KERNEL_SOURCE := 
-TARGET_KERNEL_CONFIG :=
+BOARD_KERNEL_PAGESIZE := 4096
+TARGET_KERNEL_SOURCE := kernel/toshiba/tostab03
+TARGET_KERNEL_CONFIG := kernel/toshiba/tostab03/arch/arm/configs/omni_tostab03_defconfig
 
 # Prebuilt Kernel Fallback
 # Falls back to these kernels if kernel compilation fails

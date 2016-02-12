b55e8ecddebe87a22d0aad58329f4c1f0ee84e13
TheMeddlingMonk <jwhite88@gmail.com>
2016-02-12 14:06:33 +1100

    Added placeholders for kernel source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,14 +18,16 @@ TARGET_GET_CPU_SMP := true
 TARGET_OTA_ASSERT_DEVICE := antares,tostab03
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-# Prebuilt Kernel Fallback
-TARGET_PREBUILT_KERNEL := device/toshiba/tostab03/kernel
-TARGET_PREBUILT_RECOVERY_KERNEL := device/toshiba/tostab03/kernel
-
 # Kernel
 BOARD_KERNEL_CMDLINE := nvmem=128M@384M mem=1024M@0M vmalloc=256M video=tegrafb tegra_fbmem=0x3e8a00@0x1fbfa000 console=ttyS0,115200n8 debug_uartport=hsport usbcore.old_scheme_first=1 lp0_vec=8192@0x1fbee000 tegraboot=sdmmc board_info=4249:ff41:ff:ff:54 gpt androidboot.carrier=wifi-only
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE :=
+#TARGET_KERNEL_SOURCE := 
+#TARGET_KERNEL_CONFIG :=
+
+# Prebuilt Kernel Fallback
+TARGET_PREBUILT_KERNEL := device/toshiba/tostab03/kernel
+TARGET_PREBUILT_RECOVERY_KERNEL := device/toshiba/tostab03/kernel
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/toshiba/tostab03/recovery.fstab

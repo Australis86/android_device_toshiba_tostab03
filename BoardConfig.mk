059a70cc8bdf1779b10c329eb191ff942cbc6624
TheMeddlingMonk <jwhite88@gmail.com>
2016-02-22 20:16:36 +1100

    Added more comments.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,57 +14,67 @@
 # limitations under the License.
 #
 
-# Platform
+# PLATFORM
 TARGET_BOARD_PLATFORM := tegra
 TARGET_BOOTLOADER_BOARD_NAME := antares
+# Don't use a custom bootloader
 TARGET_NO_BOOTLOADER := true
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+# Specific CPU type for ARMv7 (note for ARMv6 this is TARGET_ARCH_VARIANT_CPU)
 TARGET_CPU_VARIANT := cortex-a9
+# Enable symmetric multiprocessing
 TARGET_CPU_SMP := true
+# Architecture (i.e. arm, x86, etc.)
 TARGET_ARCH := arm
+# Architecture version (i.e. armv6, armv7, etc.)
 TARGET_ARCH_VARIANT := armv7-a
 TARGET_OTA_ASSERT_DEVICE := antares,tostab03
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-# Kernel
-BOARD_KERNEL_BASE := 0x08000000
-BOARD_KERNEL_PAGESIZE := 4096
-TARGET_KERNEL_SOURCE := kernel/toshiba/tostab03
-TARGET_KERNEL_CONFIG := omni_tostab03_defconfig
 
-# Prebuilt Kernel Fallback
-# Falls back to these kernels if kernel compilation fails
+# KERNEL
+# Address of the kernel within the boot and recovery images
+BOARD_KERNEL_BASE := 0x08000000
+# Page size of the stock boot.img and must be set correctly in order to boot - can be extracted from kernel source
+BOARD_KERNEL_PAGESIZE := 4096
+# Path to the kernel source
+TARGET_KERNEL_SOURCE := kernel/toshiba/tostab03
+# Name of the kernel configuration file
+TARGET_KERNEL_CONFIG := omni_tostab03_defconfig
+# Prebuilt kernel - used if kernel compilation fails? (TBC)
 #TARGET_PREBUILT_KERNEL := device/toshiba/tostab03/kernel
 
-# Forces uses of prebuilt kernel instead of compiled kernel
-#TARGET_PREBUILT_RECOVERY_KERNEL := device/toshiba/tostab03/recovery/kernel
 
-# Recovery
-TARGET_RECOVERY_FSTAB := device/toshiba/tostab03/recovery.fstab
-
-# Display
+# DISPLAY
 USE_OPENGL_RENDERER := true
 
-# Audio
+
+# AUDIO
 BOARD_USES_GENERIC_AUDIO := true
+
 
 # GPS
 BOARD_HAVE_GPS := true
 
-# Camera
+
+# CAMERA
 # TBA
 
-# Bluetooth
+
+# BLUETOOTH
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
+
 
 # USB
 # TBA
 
+
 # Wifi
 
-# Partitions
+
+# PARTITIONS
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
 # Technically the recovery partition is 6291456. However, the build process assumes a spare page, so 
@@ -74,20 +84,36 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 734003200
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 3221225472
 BOARD_FLASH_BLOCK_SIZE := 1024
 
-# Recovery Options
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_LARGE_FILESYSTEM := true
+
+# RECOVERY
+# Forces uses of prebuilt kernel instead of compiled kernel
+#TARGET_PREBUILT_RECOVERY_KERNEL := device/toshiba/tostab03/recovery/kernel
+# Path to custom init.rc for recovery
 #TARGET_RECOVERY_INITRC := device/toshiba/tostab03/recovery/init.rc
+# Path to custom fstab for recovery
+TARGET_RECOVERY_FSTAB := device/toshiba/tostab03/recovery.fstab
+# No select button - use power button instead
+BOARD_HAS_NO_SELECT_BUTTON := true
+# No misc partition
+BOARD_HAS_NO_MISC_PARTITION := true
+# Set if an ext4 partition is larger than 2GB
+BOARD_HAS_LARGE_FILESYSTEM := true
+
 
 # TWRP
 DEVICE_RESOLUTION := 1280x800
-#BOARD_HAS_NO_REAL_SDCARD := true # Removes SD-card partitioning (to save space if needed)
+# Removes SD-card partitioning (to save space if needed)
+#BOARD_HAS_NO_REAL_SDCARD := true
+# Enable proper handling of /data/media for devices that use this path
 RECOVERY_SDCARD_ON_DATA := true
+# Remove reboot to bootloader option from TWRP menu
 TW_NO_REBOOT_BOOTLOADER := true
+# Default to external storage instead of internal on devices with dual storage
+TW_DEFAULT_EXTERNAL_STORAGE := true
+# Flashes zips from their location rather than copying them into /tmp (useful for ROMs that are too large to fit into RAM)
+TW_FLASH_FROM_STORAGE := true
+# Mount paths
 TW_INTERNAL_STORAGE_PATH := "/data/media"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 TW_EXTERNAL_STORAGE_PATH := "/sdcard"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard"
-TW_DEFAULT_EXTERNAL_STORAGE := true
-TW_FLASH_FROM_STORAGE := true

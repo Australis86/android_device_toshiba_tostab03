5f22a2ab9c4b3376498ef482953612df1c302b21
TheMeddlingMonk <jwhite88@gmail.com>
2016-02-24 18:03:47 +1100

    Added cmdline parameter

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,8 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 
 # KERNEL
+# Commandline to pass to kernel (sourced from Dalepl's ICS ROM)
+BOARD_KERNEL_CMDLINE := nvmem=128M@384M mem=1024M@0M vmalloc=256M video=tegrafb tegra_fbmem=0x3e8a00@0x1fbfa000 console=none debug_uartport=hsport usbcore.old_scheme_first=1 lp0_vec=8192@0x1fbee000 tegraboot=sdmmc board_info=4249:ff41:ff:ff:54 gpt
 # Address of the kernel within the boot and recovery images
 BOARD_KERNEL_BASE := 0x10000000
 # Page size of the stock boot.img and must be set correctly in order to boot - can be extracted from kernel source
